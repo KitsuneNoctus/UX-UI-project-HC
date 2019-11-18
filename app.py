84abff9a74f56ba4cd32f57bc6d9a627c64fd039
KitsuneNoctus <theceintiant@gmail.com>
2019-11-18 11:06:15 -0800

    Starting on that flask

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-import flask from Flask
+from flask import Flask, render_template, request, redirect, url_for
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    """Returns Homepage"""
+    return "Hello"
+
+@app.route('/profile')
+def profile():
+    """Returns profile"""
+    return "hi"
+
+if __name__ == '__main__':
+    app.run(debug=True)
