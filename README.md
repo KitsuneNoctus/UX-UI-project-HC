16ed0295ed7fafec3178fc688b883968df7b47d4
KitsuneNoctus <theceintiant@gmail.com>
2019-11-18 11:15:51 -0800

    Adding the templates.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 The start of my SPD 1.2 Project to create the front end of a Project idea.
+Idea: Fav Tracker thing
+- Allows people to keep track of their shows, books, movies, etc.
--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     """Returns Homepage"""
-    return "Hello"
+    return render_template('home.html')
 
 @app.route('/profile')
 def profile():
