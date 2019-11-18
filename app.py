ccdea160b4aa7b40a419223640b764f0bd6d3830
KitsuneNoctus <theceintiant@gmail.com>
2019-11-18 11:44:26 -0800

    Added navbar partial

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     """Returns Homepage"""
-    return render_template('home.html')
+    return render_template('home.html', message = "hello word")
 
 @app.route('/profile')
 def profile():
